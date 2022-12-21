b8ed30466d979302973ac86bc0bd1e1f3898503e
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-21 19:50:03 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+// const body = document.querySelector('body');
+let timerID = null;
+btnStop.setAttribute('disabled', 'true');
 
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  btnStart.setAttribute('disabled', 'true');
+  btnStop.removeAttribute('disabled');
+
+  timerID = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onBtnStopClick() {
+  clearInterval(timerID);
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', 'true');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
