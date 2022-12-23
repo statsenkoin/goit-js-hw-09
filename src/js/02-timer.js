108a6cdd50790e41496be86109bedc20a91350ed
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-23 19:40:12 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,20 +1,9 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import 'dist/notiflix-3.2.5.min.css';
 
-const inputPicker = document.querySelector('#datetime-picker');
-const btnStart = document.querySelector('[data-start]');
-const fieldDays = document.querySelector('[data-days]');
-const fieldHours = document.querySelector('[data-hours]');
-const fieldMinutes = document.querySelector('[data-minutes]');
-const fieldSeconds = document.querySelector('[data-seconds]');
 let timerID = null;
-
-btnStart.setAttribute('disabled', 'true');
-
-styleTimer();
-
+let selectedDate = null;
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -27,37 +16,42 @@ const options = {
 
     if (selectedDates[0] - Date.now() > 1000) {
       btnStart.removeAttribute('disabled');
-      btnStart.addEventListener(
-        'click',
-        () => {
-          onBtnStartClick(selectedDates[0]);
-        },
-        { once: true }
-      );
+      selectedDate = selectedDates[0];
     } else {
       // window.alert('Please choose a date in the future');
       Notify.failure('Please choose a date in the future');
     }
   },
 };
+const inputPicker = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('[data-start]');
+const fieldDays = document.querySelector('[data-days]');
+const fieldHours = document.querySelector('[data-hours]');
+const fieldMinutes = document.querySelector('[data-minutes]');
+const fieldSeconds = document.querySelector('[data-seconds]');
 
+btnStart.addEventListener('click', onBtnStartClick, { once: true });
+btnStart.setAttribute('disabled', 'true');
+styleTimer(); // styles our timer a bit
 flatpickr(inputPicker, options);
 
-function onBtnStartClick(selectedDates) {
+function onBtnStartClick() {
   inputPicker.setAttribute('disabled', 'true');
   btnStart.setAttribute('disabled', 'true');
 
   timerID = setInterval(() => {
-    const timeLeftMs = selectedDates - Date.now();
+    const timeLeftMs = selectedDate - Date.now();
     const timeLeft = convertMs(timeLeftMs);
+
+    if (timeLeftMs < 1000) {
+      clearInterval(timerID);
+    }
+
     fieldDays.textContent = addLeadingZero(timeLeft.days);
     fieldHours.textContent = addLeadingZero(timeLeft.hours);
     fieldMinutes.textContent = addLeadingZero(timeLeft.minutes);
     fieldSeconds.textContent = addLeadingZero(timeLeft.seconds);
 
-    if (timeLeftMs < 1000) {
-      clearInterval(timerID);
-    }
     console.log('timeLeft :>> ', timeLeft);
   }, 1000);
 }
@@ -85,6 +79,7 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
+// adds styles to our timer
 function styleTimer() {
   const timerEl = document.querySelector('.timer');
   const fieldEl = document.querySelectorAll('.field');
@@ -99,7 +94,3 @@ function styleTimer() {
   });
   valueEl.forEach(el => (el.style.fontSize = '52px'));
 }
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
